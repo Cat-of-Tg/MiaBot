172aeb2f73741e63d4f6f09d6c98c6c976ad2be5
POISON <86126808+PoisonBot@users.noreply.github.com>
2021-08-25 19:41:31 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -140,8 +140,8 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
                     [[InlineKeyboardButton(text=" ADD ME TO UR GROUP", url="t.me/{}?startgroup=true".format(bot.username))],  [InlineKeyboardButton(text="UPDATES", url="https://t.me/team_lad")],
                      [InlineKeyboardButton(text="SUPPORT", url="https://t.me/teamladz_bothub"), InlineKeyboardButton(text="DEV", url="https://t.me/OK_BIE_BOT")],
-                     [InlineKeyboardButton(text=" HELP", url="https://t.me/{}?start=help".format(bot.username)) ]]))
-
+                     [InlineKeyboardButton(text=" HELP", callback_data="help_back").format(bot.username)) ]]))
+ 
     else:
         update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
 
